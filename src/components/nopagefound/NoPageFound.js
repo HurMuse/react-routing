42c84508a931797177f8f420f71ef6ddf84fd8b1
HurMuse <caremplob@gmail.com>
2021-02-05 10:57:12 -0600

    file was locked

---
+++ src/components/nopagefound/NoPageFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoPageFound(props) {
+  return (
+    <div>
+      <h3>Sorry No Page Found! </h3>
+    </div>
+  );
+}
+
+export default NoPageFound;
---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Welcome(props) {
+  console.log(props);
+  let name =
+    (props.match && props.match.params.name) ||
+    props.name ||
+    "No name provided";
+  return <div className="Welcome">Welcome, {name}!</div>;
+}
+
+export default Welcome;
